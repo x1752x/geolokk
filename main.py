221fcbad9d51195a38697a7c44c75e083721f2c3
Vladislav <vlad_kadun@vk.com>
2026-04-18 03:41:12 +0700

    bind app to PostgreSQL database
    1. SensorCluster now works in asynchronous worker
    2. SensorCluster state is now one for all clients without duplicating
    3. SensorCluster now writes into database, not to Client
    4. Clients now subscribing on updates via PostgreSQL LISTEN-NOTIFY feature

--- main.py
+++ main.py
@@ -2,36 +2,89 @@ from fastapi import FastAPI, Request, WebSocket, WebSocketDisconnect
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
+from contextlib import asynccontextmanager
+
+from concurrent.futures import ThreadPoolExecutor
 
 import asyncio
+import psycopg2
+import yaml
+import threading
+import json
+import time
+import asyncpg
 
 from GaussianNoiseGenerator import *
 from BerlageImpulse import *
 from Sensor import *
 from SensorCluster import *
 
-app = FastAPI()
+with open("config.yaml", 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+connection = psycopg2.connect(dbname=config["database"], 
+                    user=config["user"], 
+                    password=config["password"], 
+                    host=config["host"],
+                    port=config["port"])
+
+with connection.cursor() as cursor:
+    cursor.execute("SELECT * FROM cluster_structure")
+    structure = cursor.fetchall()
+
+    cluster = SensorCluster([Sensor([sensor[1], sensor[2]], sensor[0]) for sensor in structure])
+
+    cursor.execute("SELECT * FROM noise_source")
+    source_layout = cursor.fetchall()[0]
+
+    source = NoiseSource([source_layout[1], source_layout[2]], source_layout[0])
+
+connection.close()
+
+def worker(stop_event: threading.Event):
+    connection = psycopg2.connect(dbname=config["database"], 
+                    user=config["user"], 
+                    password=config["password"], 
+                    host=config["host"],
+                    port=config["port"])
+    cursor = connection.cursor()
+
+    while not stop_event.is_set():
+        if cluster.get(10).next_event == -1 and cluster.get(11).next_event == -1:
+            cluster.record(source)
+            #print(f"RECORD CALLED at t={cluster.get(10).t}")
+        data = cluster.generate_once()
+        json_data = json.dumps(data)
+        cursor.execute("INSERT INTO cluster_responses (response) VALUES (%s);", (json_data,))
+        cursor.execute("""
+            DELETE FROM cluster_responses
+            WHERE id IN (
+                SELECT id FROM cluster_responses
+                ORDER BY id ASC
+                LIMIT (SELECT GREATEST(0, count(*) - 2000) FROM cluster_responses)
+            )
+        """)
+        connection.commit()
+        time.sleep(0.01)
+
+    cursor.close()
+    connection.close()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    stop = threading.Event()
+    thread = threading.Thread(target = worker, args=(stop,), daemon=True)
+    print("WORKER THREAD STARTING...")
+    thread.start();
+
+    yield
+
+    stop.set()
+
+app = FastAPI(lifespan=lifespan)
 templates = Jinja2Templates(directory='templates')
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
-
-cluster = SensorCluster([
-    Sensor([0, 0],  0),
-    Sensor([0, 600],  1),
-    Sensor([0, 1200],  2),
-    Sensor([0, -600], 3),
-    Sensor([0, -1200], 4),
-    Sensor([600, 0],  5),
-    Sensor([1200, 0],  6),
-    Sensor([-600, 0], 7),
-    Sensor([-1200, 0], 8),
-    Sensor([-1800, 0], 9),
-    Sensor([1800, 0], 10),
-    Sensor([0, -1800], 11),
-    Sensor([0, 1800],  12),
-])
-
-source = NoiseSource([-600, 600], 0)
 
 @app.get('/', response_class = HTMLResponse)
 def index(request: Request):
@@ -46,33 +99,29 @@ def index(request: Request):
 
 @app.websocket("/noise")
 async def noise(*, ws: WebSocket):
-    cluster = SensorCluster([
-        Sensor([0, 0],  0),
-        Sensor([0, 600],  1),
-        Sensor([0, 1200],  2),
-        Sensor([0, -600], 3),
-        Sensor([0, -1200], 4),
-        Sensor([600, 0],  5),
-        Sensor([1200, 0],  6),
-        Sensor([-600, 0], 7),
-        Sensor([-1200, 0], 8),
-        Sensor([-1800, 0], 9),
-        Sensor([1800, 0], 10),
-        Sensor([0, -1800], 11),
-        Sensor([0, 1800],  12),
-    ])
-
-    source = NoiseSource([-600, 600], 0)
-
     await ws.accept()
+    
+    conn = await asyncpg.connect(
+        user=config['user'],
+        password=config['password'],
+        host=config['host'],
+        database=config['database'],
+        port=config['port']
+    )
+    
+    queue = asyncio.Queue()
+
+    async def callback(conn, pid, channel, payload):
+        await queue.put(payload)
+
+    await conn.add_listener('new_response_channel', callback)
+
     try:
         while True:
-            if cluster.get(10).next_event == -1 and cluster.get(11).next_event == -1:
-                cluster.record(source)
-            data = cluster.generate_once()
-            
-            await ws.send_json(data)
-            await asyncio.sleep(0.01)
-            
+            payload = await queue.get()
+            await ws.send_text(payload)
     except WebSocketDisconnect:
         pass
+    finally:
+        await conn.remove_listener('new_response_channel', callback)
+        await conn.close()
